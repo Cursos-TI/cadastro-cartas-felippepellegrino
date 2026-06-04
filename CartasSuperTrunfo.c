9548c01234e854b6748fa5b7d404dafca9affc22
felippepellegrino <166524899+felippepellegrino@users.noreply.github.com>
2026-06-04 15:01:37 +0000

    Fiz o desafio nível novato, adicionei variaveis , entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,51 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado[50];
+    char CodigoCarta[50];
+    char Cidade[100];
+    int Populacao;
+    float Area;
+    float Pib;
+    int NumPontosTuristicos;
 
   // Área para entrada de dados
+    printf("Digite o estado: ");
+    scanf("%s", Estado);
+
+    printf("Digite o código da carta: ");
+    scanf("%s", CodigoCarta);
+
+    printf("Cidade: ");
+    scanf("%s", Cidade);
+
+    printf("População: ");
+    scanf("%i", &Populacao);
+
+    printf("Área Km²: ");
+    scanf("%f", &Area);
+
+    printf("PIB: ");
+    scanf("%f", &Pib);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%i", &NumPontosTuristicos);
+
 
   // Área para exibição dos dados da cidade
+    printf("Estado: %s\n", Estado);
+
+    printf("Código da carta: %s\n", CodigoCarta);
+
+    printf("Cidade: %s\n", Cidade);
+
+    printf("População: %i\n", Populacao);
+
+    printf("Área: %f\n", Area);
+
+    printf("PIB: %.2f km²\n", Pib);
+
+    printf("Número de pontos turísticos: %i\n", NumPontosTuristicos);
 
 return 0;
 } 
